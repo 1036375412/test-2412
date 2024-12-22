d48d33bb74754c75503ffebf24429bc37335a994
1036375412 <1036375412@qq.com>
2024-12-23 01:43:19 +0800

    Fi

--- test_switch_case/test.c
+++ test_switch_case/test.c
@@ -7,28 +7,28 @@ int main()
 	switch (day)
 	{
 	case 1:
-		printf("Monday\n");
+		printf("星期1\n");
 		break;
 	case 2:
-		printf("Tuesday\n");
+		printf("星期2\n");
 		break;
 	case 3:
-		printf("Wednesday\n");
+		printf("星期3\n");
 		break;
 	case 4:
-		printf("Thursday\n");
+		printf("星期4\n");
 		break;
 	case 5:
-		printf("Friday\n");
+		printf("星期5\n");
 		break;
 	case 6:
-		printf("Saturday\n");
+		printf("星期6\n");
 		break;
 	case 7:
-		printf("Sunday\n");
+		printf("星期7\n");
 		break;
 	default:
-		printf("Wrong!\n");
+		printf("输入错误\n");
 	}
 	return 0;
 }
